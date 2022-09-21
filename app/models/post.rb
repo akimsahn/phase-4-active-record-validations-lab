fedd923bc089a0e5109f758f740fa9669e7d5fc2
Allison Kim <akeem159@yahoo.com>
2022-09-21 16:12:34 -0400

    commit tree f3b0fc0513624f3980a8f45a73d9e274aece7a9a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+    validate :title_must_be_clickbaity
+
+    def title_must_be_clickbaity
+        unless title && ["Won't Believe", "Secret", "Top", "Guess"].any? { |keyword| title.include? keyword }
+            errors.add(:title, 'Title is not clickbait-y')
+        end
+    end
 end
